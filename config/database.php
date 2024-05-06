aee3f21bf56d89858a76f4dc52d82e2eb8f1c1d5
Lester Fong <lester.fong@switchconnect.com.au>
2024-05-06 21:06:59 +0800

    Update database configuration to use variables for hostname, database name, username, and password

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+
+$hostname = "127.0.0.1";
+$dbname = "librarysystem"; // change this to the name of your database
+$dbusername = "root";
+$dbpassword = "";
+
 return [
 
     /*
@@ -46,11 +52,15 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $hostname,
+            // 'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $dbname,
+            'username' => $dbusername,
+            'password' => $dbpassword,
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
