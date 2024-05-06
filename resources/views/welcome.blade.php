ec4741bb90429f37db090000a119daa7763dcace
Lester Fong <lester.fong@switchconnect.com.au>
2024-05-06 20:59:42 +0800

    Update welcome.blade.php to remove unnecessary meta tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel8 - Vue3</title>
     </head>
     <body >
