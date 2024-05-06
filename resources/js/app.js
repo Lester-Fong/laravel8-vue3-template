4b75a53a50fd652298ff00e7209844bc4043fc15
Lester Fong <lester.fong@switchconnect.com.au>
2024-05-06 20:59:51 +0800

    Refactor app.js to improve code readability and organization

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,21 @@
-require("./bootstrap");
-
 import { createApp } from "vue";
 import { createWebHistory, createRouter } from "vue-router";
 
 import App from "./App.vue";
 import routes from "./router";
+require("./bootstrap");
 
-console.log(routes);
-
+// creating the app and reding the App.vue file
 const app = createApp(App);
 
-const router = new createRouter({
+// creating the router and reading the routes file
+const router = createRouter({
     history: createWebHistory(),
-    routes, // short for `routes: routes`
+    routes,
 });
 
-app.use(router).mount("#app");
+// using the router
+app.use(router);
+
+// Vue3's way of mounting the app to the div with id app which is in the Welcome.blade.php file
+app.mount("#app");
